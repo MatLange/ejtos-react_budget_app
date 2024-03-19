b5671574eefc9617df362ead13feea2e14fa3a48
MatLange <matti.lange@gmx.de>
2024-03-19 07:29:44 -0400

    Update currency dropdown

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,26 +1,35 @@
 import React, { useContext, useState } from 'react';
+import './Currency.css';
 import { AppContext } from '../context/AppContext';
+
 const Currency = () => {
     const { currency } = useContext(AppContext);
     const [newCurrency, setNewCurrency] = useState(currency);
 
     const handleCurrencyChange = (event) => {
         setNewCurrency(event.target.value);
+        if (event.target.parentElement) {
+        event.target.parentElement.value = "HHHAHHA";
+        }
     }    
     const alertType = 'alert-success';
     return (
-        <div className={`alert ${alertType}`}>
-            <div className="input-group-prepend">
-            <span className="text-white">Currency</span>
-            </div>
-            <select className={`alert ${alertType}`} id="inputGroupSelect03" onChange={handleCurrencyChange}>
-            <option defaultValue>Choose...</option>
-            <option value="$" name="dollar">$ Dollar</option>
-            <option value="£" name="pound">£ Pound</option>
-            <option value="€" name="pound">€ Euro</option>
-            <option value="₹" name="pound">₹ Ruppee</option>
-            </select>
-        </div>
+<div class="container">
+  <h2>Dropdowns</h2>
+  <p>The .dropdown class is used to indicate a dropdown menu.</p>
+  <p>Use the .dropdown-menu class to actually build the dropdown menu.</p>                                          
+  <div class="dropdown">
+    <button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown">Tutorials
+    <span class="caret"></span></button>
+    <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
+      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">HTML</a></li>
+      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">CSS</a></li>
+      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">JavaScript</a></li>
+      <li role="presentation" class="divider"></li>
+      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">About Us</a></li>
+    </ul>
+  </div>
+</div>
     );
 };
 export default Currency;
