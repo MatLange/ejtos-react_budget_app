c5478d697ab74ca6e01e465b7e8c6bb033e9db70
MatLange <matti.lange@gmx.de>
2024-03-19 11:29:22 -0400

    Update currency dropdown

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,36 +1,46 @@
 import React, { useContext, useState } from 'react';
 import './Currency.css';
 import { AppContext } from '../context/AppContext';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 
 const Currency = () => {
     const { currency } = useContext(AppContext);
     const [newCurrency, setNewCurrency] = useState(currency);
+    
+    const determineDropdownTitle = (newCurrency) => {
+        const dropdownTitles = {
+            "$": "$ Dollar",
+            "£": "£ Pound",
+            "€": "€ Euro",
+            "₹": "₹ Ruppee",
+        };            
+        const dropdownTitle = dropdownTitles[newCurrency || "£"];
+        return "Currency(" + dropdownTitle + ")";
+    };
 
-    const handleCurrencyChange = (event) => {
-        setNewCurrency(event.target.value);
-        if (event.target.parentElement) {
-        event.target.parentElement.value = "HHHAHHA";
-        }
+    const [dropdownTitle, setDropdownTitle] = useState(determineDropdownTitle(currency));    
+
+    const handleCurrencyChange = (event, newCurrencyValue) => {
+        console.log(newCurrencyValue);
+        setNewCurrency(newCurrencyValue);
+        const newDropdownTitle = determineDropdownTitle(newCurrencyValue);
+        setDropdownTitle(newDropdownTitle);
     }    
+
     const alertType = 'alert-success';
     return (
-<div class="container">
-  <h2>Dropdowns</h2>
-  <p>The .dropdown class is used to indicate a dropdown menu.</p>
-  <p>Use the .dropdown-menu class to actually build the dropdown menu.</p>                                          
-  <div class="dropdown">
-    <button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown">Tutorials
-    <span class="caret"></span></button>
-    <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
-      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">HTML</a></li>
-      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">CSS</a></li>
-      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">JavaScript</a></li>
-      <li role="presentation" class="divider"></li>
-      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">About Us</a></li>
-    </ul>
-  </div>
-</div>
+        <div className='alert alert-secondary'>
+            <DropdownButton  id="dropdown-basic-button" variant="success" title={dropdownTitle}>
+                <Dropdown.Item onClick={event => handleCurrencyChange(event, "$" )} eventKey="$">$ Dollar</Dropdown.Item>
+                <Dropdown.Item onClick={event => handleCurrencyChange(event, "£" )} eventKey="pound">£ Pound</Dropdown.Item>
+                <Dropdown.Item onClick={event => handleCurrencyChange(event, "€" )} eventKey="euro">€ Euro</Dropdown.Item>
+                <Dropdown.Item onClick={event => handleCurrencyChange(event, "₹" )} eventKey="ruppee">₹ Ruppee</Dropdown.Item>
+            </DropdownButton>  
+        </div>
     );
 };
 export default Currency;
+
 
